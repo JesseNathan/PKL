9965f436b06c9a483b6cbaa40acd7ea49bbdca76
JesseNathan <rickumjesse@gmail.com>
2025-08-13 09:02:57 +0700

    first commit

--- src/Pasien.jsx
+++ src/Pasien.jsx
@@ -9,20 +9,20 @@ function Pasien() {
       
       {/* Logo */}
           <div className="relative top-[-65px] gap-240 flex max-md:flex-col">
-          <div className="max-md:ml-5 max-md:w-full">
+            <div className="max-md:ml-5 max-md:w-full">
             <img
                src="https://cdn.builder.io/api/v1/image/assets/be69b2afbbb64a02ad678735c20641c6/002dd8f3dbbb902ec73724109be5d3ada5aba01f?placeholderIfAbsent=true"
                alt="Company logo"
-               className="object-contain mt-10 max-w-full aspect-[2.41] w-[250px] max-md:mt-10 ml-[-10px]"
+               className="object-contain mt-10 max-w-full aspect-[2.41] w-[150px] max-md:mt-10 ml-[-10px]"
              />
-           </div>
-           <div className="ml-5 w-max-md:ml-0 max-md:w-full">
+            </div>
+            <div className="ml-5 w-max-md:ml-0 max-md:w-full">
                <img
                  src="https://cdn.builder.io/api/v1/image/assets/be69b2afbbb64a02ad678735c20641c6/37b8072c7fcb0d31139cb33103e19b2e0aad1249?placeholderIfAbsent=true"
                  alt="Healthcare icon"
-                 className="object-contain mt-10 max-w-full aspect-[2.41] w-[250px] max-md:mt-10 ml-[-10px]0 "
+                 className="object-contain mt-10 max-w-full aspect-[2.41] w-[200px] max-md:mt-10 ml-[-10px]0 "
                />
-             </div>
+            </div>
          </div>
 
       {/* Menu utama */}
